2e2331f6d545e8473daf5bc969a2f61e98305d51
RebecaAlvesLima <cc21448@g.unicamp.br>
2023-09-20 16:51:33 -0300

    arrumando o logo

--- src/components/template/Logo.js
+++ src/components/template/Logo.js
@@ -1,9 +1,13 @@
 import './Logo.css';
+import React from 'react';
+import cotuca from '../../assets/cotuca.png'
 
 export default function Logo(props){
     return(
         <aside className="logo">
-            Logo
+            <a href='/' className='logo'>
+                <img className= "img" src={cotuca} alt="Logo"/>
+            </a>
         </aside>
     )
 }
