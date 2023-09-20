0be84427997b979c8282f48904d5b139c130b12d
RebecaAlvesLima <cc21448@g.unicamp.br>
2023-09-20 16:27:53 -0300

    app css

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import './App.css';
 import Logo from './components/template/Logo';
 import Menu from './components/template/Menu';
 import Main from './components/template/Main';
+import Header from './components/template/Header';
 import Footer from './components/template/Footer';
 
 function App() {
   return (
     <div className="App">
-      <h1>Projeto Escola</h1>
+      <Header />
+      <Logo />
+      <Menu />
+      <Main />
+      <Footer />
 
     </div>
   );
