6ca5a02936a9f133ca2eb685363ee4e10a828491
RebecaAlvesLima <cc21448@g.unicamp.br>
2023-09-20 15:45:28 -0300

    import main, menu, logo e footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Logo from './components/template/Logo';
+import Menu from './components/template/Menu';
+import Main from './components/template/Main';
+import Footer from './components/template/Footer';
 
 function App() {
   return (
     <div className="App">
       <h1>Projeto Escola</h1>
-      
+
     </div>
   );
 }
